5cda4ab3c2c3fda2b034d885b84e70132d669e29
Rakhyunn <krh0323@pukyong.ac.kr>
2026-04-22 16:12:11 +0900

    [feat] t3 green

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,17 +1,19 @@
 package com.ll.framework.ioc;
 
+import com.ll.domain.testPost.repository.TestPostRepository;
 import com.ll.domain.testPost.service.TestPostService;
 
-public class ApplicationContext {
-    public ApplicationContext() {
+import java.util.HashMap;
+import java.util.Map;
 
+public class ApplicationContext {
+    Map<String, Object> beans = new HashMap<>();
+
+    public ApplicationContext() {
+        beans.put("testPostService", new TestPostService(new TestPostRepository()));
     }
 
     public <T> T genBean(String beanName) {
-        if(beanName.equals("testPostService")) {
-            TestPostService testPostService = new TestPostService(genBean("testPostRepository"));
-            return (T) testPostService;
-        }
-        return (T) null;
+        return (T) beans.get(beanName);
     }
 }
